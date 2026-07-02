e3f5d7257835be864859c9f24e1bf5ca0f16663e
Apart004 <jagdhariansh2004@gmail.com>
2026-07-02 17:26:47 +0530

    ci: add OWASP ZAP DAST scanner to pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,15 +88,6 @@ resource "aws_security_group" "ecommerce_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # VULNERABILITY: SSH open to entire internet (intentional for milestone test)
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
